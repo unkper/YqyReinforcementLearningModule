1d0516b3e0b33418dd40c298b67e5d09b0a4dc97
yangqingyang0428@163.com <yangqingyang0428@163.com>
2023-01-16 10:27:09 +0800

    添加mario训练环境

---
+++ rl_platform/tianshou_case/mario/mario_test.py
@@ -0,0 +1,14 @@
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+done = True
+for step in range(5000):
+    if done:
+        state = env.reset()
+    state, reward, done, info = env.step(env.action_space.sample())
+    env.render()
+
+env.close()

1ccc2fc3a1e0858fdeea87cdc9de471ec66be9a3
yangqingyang0428@163.com <yangqingyang0428@163.com>
2023-03-23 00:19:55 +0800

    Update run_car_racing_ppo.py

--- rl_platform/tianshou_case/standard_gym/run_car_racing_ppo.py
+++ rl_platform/tianshou_case/standard_gym/run_car_racing_ppo.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import logging
 import os
 import pprint
@@ -23,32 +24,37 @@ import sys
 from tianshou.utils.net.discrete import Actor, Critic, IntrinsicCuriosityModule
 from torch.optim import Adam, Optimizer
 
+from rl_platform.tianshou_case.mario.wrapper import create_mario_env
+from rl_platform.tianshou_case.net.mario_net import MarioFeatureNet, MarioICMHead
 from rl_platform.tianshou_case.net.r_network import RNetwork
-from rl_platform.tianshou_case.net.standard_net import CarRacingPolicyHead, CarRacingICMHead
-from rl_platform.tianshou_case.standard_gym.wrapper import create_car_racing_env, RewardType
+from rl_platform.tianshou_case.standard_gym.wrapper import RewardType, create_car_racing_env
 from rl_platform.tianshou_case.third_party import r_network_training
 from rl_platform.tianshou_case.third_party.episodic_memory import EpisodicMemory
+from rl_platform.tianshou_case.utils.common import CustomJSONEncoder
 
 sys.path.append(r"D:\projects\python\PedestrainSimulationModule")
 
-parallel_env_num = 8
-test_env_num = 4
-episode_per_test = 4
-lr, gamma, n_steps = 2.5e-4, 0.99, 3
-buffer_size = 200000
-batch_size = 128
+#环境配置相关
+parallel_env_num = 5
+test_env_num = 5
+train_reward_type = RewardType.RAW_REWARD
+level = "1-2"
+#PPO算法相关
+episode_per_test = 20
+lr, gamma, n_steps = 1e-4, 0.99, 3
+buffer_size = 100000
+batch_size = 64
 eps_train, eps_test = 0.2, 0.05
-max_epoch = 60
+max_epoch = 120
 step_per_epoch = 10000
 update_policy_interval = 1000
-repeat_per_collect = 4
-rew_norm = True
+update_repeat_count = 4
+rew_norm = False
 vf_coef = 0.25
 ent_coef = 0.01
-gae_lambda = 0.95
-lr_decay = True
+gae_lambda = 0  # 不再使用gae算法计算TD误差
 lr_scheduler = None
-max_grad_norm = 0.5
+max_grad_norm = 40
 eps_clip = 0.1
 dual_clip = None
 value_clip = 1
@@ -56,39 +62,40 @@ norm_adv = 1
 recompute_adv = 0
 update_per_step = 0.1
 hidden_size = 100
-
-actor_lr = lr
 set_device = "cuda"
-env_name = "normal"
 # icm parameters
-use_icm = True
+use_icm = False
 icm_hidden_size = 256
-icm_lr_scale = 1e-3
+icm_lr_scale = 10
 icm_reward_scale = 0.1
 icm_forward_loss_weight = 0.2
 # EC parameters
-use_episodic_memory = False
+use_episodic_memory = True
 exploration_reward = "episodic_curiosity"  # episodic_curiosity,oracle
 scale_task_reward = 1.0
-scale_surrogate_reward = 5.0  # 5.0 for vizdoom in ec,指的是EC奖励的放大倍数
-bonus_reward_additive_term = 0
+scale_ec_reward = 2.0  # 5.0 for vizdoom in ec,指的是EC奖励的放大倍数
+bonus_reward_additive_term = 0 # 0.5 - curiosity_reward + term
 exploration_reward_min_step = 0  # 用于在线训练，在多少步时加入EC的相关奖励
 similarity_threshold = 0.5
 target_image_shape = [96, 96, 4]  # [96, 96, 4个连续灰度图像的堆叠]
-r_network_checkpoint = r"D:\Projects\python\PedestrainSimlationModule\rl_platform\tianshou_case\vizdoom\checkpoints\VizdoomMyWayHome-v0_PPO_2023_03_11_01_35_53\r_network_weight_500.pt"
+r_network_checkpoint = r"D:\Projects\python\PedestrainSimlationModule\rl_platform\tianshou_case\mario\r_network\Mario_v3_PPO_2023_03_20_00_00_25\r_network_weight_650.pt"
 # EC online train parameters
 use_EC_online_train = False
 v_r_network = None
 memory = None
 r_trainer = None
 # 文件配置相关
-task = "CarRacing_{}".format(env_name)
+env_name = "normal"
+task = "Mario_{}".format(env_name)
 file_name = task + "_PPO_" + datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+if use_episodic_memory and use_icm:
+    raise RuntimeError()
 
 
 def reset_train():
     global file_name, v_r_network, memory, r_trainer
     file_name = task + "_PPO_" + datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
     if use_episodic_memory:
         v_r_network = RNetwork(target_image_shape, device=set_device)
         if set_device == 'cuda':
@@ -116,8 +123,8 @@ def get_policy(env, optim=None):
     action_shape = env.action_space.shape or env.action_space.n
 
     h, w, c = target_image_shape
-    # net = DQN(**cfg.policy.model)
-    net = CarRacingPolicyHead(c, h, w, device=set_device)
+
+    net = MarioFeatureNet(c, h, w, device=set_device)
 
     if set_device == "cuda":
         net.cuda()
@@ -147,14 +154,14 @@ def get_policy(env, optim=None):
         action_space=env.action_space,
         eps_clip=eps_clip,
         value_clip=value_clip,
-        dual_clip=dual_clip,
+        dual_clip=None,
         advantage_normalization=norm_adv,
         recompute_advantage=recompute_adv,
     ).to(set_device)
 
     if use_icm:
         logging.warning(u"使用了ICM机制!")
-        feature_net = CarRacingICMHead(c, h, w, device=set_device)
+        feature_net = MarioICMHead(c, h, w, device=set_device)
         if set_device == "cuda":
             feature_net.cuda()
 
@@ -167,7 +174,7 @@ def get_policy(env, optim=None):
             hidden_sizes=[icm_hidden_size, 100],
             device=set_device,
         )
-        icm_optim = torch.optim.Adam(icm_net.parameters(), lr=actor_lr)
+        icm_optim = torch.optim.Adam(icm_net.parameters(), lr=lr)
         policy = ICMPolicy(
             policy, icm_net, icm_optim, icm_lr_scale, icm_reward_scale,
             icm_forward_loss_weight
@@ -204,8 +211,8 @@ def _get_train_env():
 
 
 def _get_test_env():
-    env = create_car_racing_env(zero_reward=RewardType.RAW_REWARD)
-    return _get_env(env)
+    env = create_car_racing_env()
+    return env
 
 
 def _get_env(env):
@@ -223,7 +230,7 @@ def _get_env(env):
             target_image_shape,
             # r_net_trainer=r_trainer,
             scale_task_reward=scale_task_reward,
-            scale_surrogate_reward=scale_surrogate_reward,
+            scale_surrogate_reward=scale_ec_reward,
             exploration_reward_min_step=exploration_reward_min_step,
             test_mode=env_test
         )
@@ -237,12 +244,11 @@ def train(load_check_point=None):
         parallel_env_num, test_env_num, buffer_size = 2, 1, 10000
         max_epoch = 2
         step_per_epoch = 100
-        step_per_collect = 10
+        update_policy_interval = 10
         episode_per_test = 1
         batch_size = 16
-
     ppo_train(batch_size, buffer_size, episode_per_test, load_check_point, max_epoch, parallel_env_num,
-              step_per_collect, step_per_epoch, test_env_num)
+              update_policy_interval, step_per_epoch, test_env_num)
 
 
 def ppo_train(batch_size, buffer_size, episode_per_test, load_check_point, max_epoch, parallel_env_num,
@@ -268,6 +274,10 @@ def ppo_train(batch_size, buffer_size, episode_per_test, load_check_point, max_e
     writer = SummaryWriter('log/' + file_name)
     logger = ts.utils.TensorboardLogger(writer)  # TensorBoard is supported!
 
+    params_dict = globals()
+    f = open(os.path.join("log", file_name, "params.json"), "w")
+    json.dump(params_dict, f, indent=4, cls=CustomJSONEncoder)
+
     def save_best_fn(policy):
         model_save_path = os.path.join('log/' + file_name, "policy.pth")
         os.makedirs(os.path.join('log/' + file_name), exist_ok=True)
@@ -283,11 +293,11 @@ def ppo_train(batch_size, buffer_size, episode_per_test, load_check_point, max_e
 
     # def stop_fn(mean_rewards):
     #     return mean_rewards >= 2500
-    def train_fn(epoch, env_step):
-        policy.set_eps(eps_train)
-
-    def test_fn(epoch, env_step):
-        policy.set_eps(eps_test)
+    # def train_fn(epoch, env_step):
+    #     policy.set_eps(eps_train)
+    #
+    # def test_fn(epoch, env_step):
+    #     policy.set_eps(eps_test)
 
     def reward_metric(rews):
         return rews[:]
@@ -300,12 +310,12 @@ def ppo_train(batch_size, buffer_size, episode_per_test, load_check_point, max_e
         max_epoch=max_epoch,
         step_per_epoch=step_per_epoch,
         step_per_collect=step_per_collect,
-        repeat_per_collect=repeat_per_collect,
+        repeat_per_collect=update_repeat_count,
         episode_per_test=episode_per_test,
         batch_size=batch_size,
         # train_fn=train_fn,
         # test_fn=test_fn,
-        # stop_fn=stop_fn,
+        #stop_fn=stop_fn,
         save_best_fn=save_best_fn,
         save_checkpoint_fn=save_checkpoint_fn,
         update_per_step=update_per_step,
@@ -316,34 +326,43 @@ def ppo_train(batch_size, buffer_size, episode_per_test, load_check_point, max_e
     pprint.pprint(result)
 
 
-def test():
+def test(ep=5):
     global env_test
     env_test = True
-    policy_path = r"D:\Projects\python\PedestrainSimlationModule\rl_platform\tianshou_case\vizdoom\log\Vizdoom_normal_PPO_2023_03_11_23_01_22\checkpoint_100.pth"
+    policy_path = r"D:\Projects\python\PedestrainSimlationModule\rl_platform\tianshou_case\mario\log\Mario_normal_PPO_2023_03_20_00_48_28\checkpoint_120.pth"
     test_envs = DummyVectorEnv([_get_test_env for _ in range(1)])
     env = _get_test_env()
     policy, optim, agents = _get_agent(env, None,
                                        file_path=policy_path)
     policy.eval()
     collector = Collector(policy, test_envs)
-    res = collector.collect(n_episode=2, render=1 / 36)
+    res = collector.collect(n_episode=ep, render=1 / 36)
     pprint.pprint(res)
 
 
 def icm_one_experiment():
-    global use_icm
+    global use_icm, use_episodic_memory
     # use_icm
+    use_icm = True
+    use_episodic_memory = False
     train()
     # not use_icm
     use_icm = False
+    use_episodic_memory = False
     train()
+    #use ec
+    use_icm = False
+    use_episodic_memory = True
+    # train()
 
 
 debug = False
 
 if __name__ == "__main__":
-    time.sleep(7800)
+    # time.sleep(7800)
     icm_one_experiment()
+    #train()
+    # test()
 
     # train_if = True
     #

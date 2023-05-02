7697b8ce0487aea6175b4d487898778fbbc6ef6d
unkper <33563150+unkper@users.noreply.github.com>
2023-05-02 15:10:15 +0800

    20230502

--- third_party/maicm/analyze/func.py
+++ third_party/maicm/analyze/func.py
@@ -32,7 +32,7 @@ def random_noise(data: pd.DataFrame, column_name, range=(-1, 1)):
     return data
 
 
-def draw_arrive_plot(path_dir, label_type=0, window=30, range=(-1, 1)):
+def draw_arrive_plot(path_dir, label_type=0, window=1, mrange=(-1, 1)):
     labels = {
         0: ["with_intrinsic_reward",
             "without_intrinsic_reward"],
@@ -58,23 +58,43 @@ def draw_arrive_plot(path_dir, label_type=0, window=30, range=(-1, 1)):
         except FileNotFoundError:
             continue
 
-    def _inner(y_label="total_n_found_exit"):
+    def _inner(y_label="total_n_found_exit", save_pth=path_dir):
         fig, ax = plt.subplots()
 
         for fr, label in zip(dataframes, labels[label_type]):
             # 绘制图表
-            ax.plot(fr['timestep'], fr[y_label].rolling(window).mean(), label=label)
+            if window != 1:
+                ax.plot(fr['timestep'], fr[y_label].rolling(window).mean(), label=label)
+            else:
+                ax.plot(fr['timestep'], fr[y_label], label=label)
         ax.set_xlabel('time_step')
         ax.set_ylabel(y_label.replace('/', '_'))
         ax.legend(loc="best")
 
-        plt.savefig(os.path.join(path_dir, "plot_{}.png".format(y_label.replace("/", "_"))))
+        plt.savefig(os.path.join(save_pth, "plot_{}.png".format(y_label.replace("/", "_"))))
 
-    _inner("total_n_found_exit")
-    _inner("episode_rewards/extrinsic/mean")
-    _inner("episode_lengths/mean")
+    s_pth = path_dir
+    _inner("total_n_found_exit", s_pth)
+    _inner("episode_rewards/extrinsic/mean", s_pth)
+    _inner("episode_lengths/mean", s_pth)
+
+    # 开始绘制500，1000，2000步数的折线图
+    for st in [500, 1000, 2000]:
+        dataframes = []
+        s_pth = os.path.join(path_dir , str(st))
+        if not os.path.exists(s_pth):
+            os.makedirs(s_pth)
+        for subfolder in subfolders:
+            pth = os.path.join(path_dir, subfolder, r"data/main_step{}.xlsx".format(st))
+            try:
+                frame = pd.read_excel(pth)
+                dataframes.append(frame)
+            except FileNotFoundError:
+                continue
+        _inner("n_found_exits/extrinsic/mean", s_pth)
+        _inner("step_rewards/extrinsic/mean", s_pth)
 
 
 if __name__ == '__main__':
-    pth = r"/homed/projects/YqyReinforcementLearningModule/third_party/data/compare_explore_types/map_09_4agents_taskleave/2023_04_26_21_07_51exp_test"
-    draw_arrive_plot(pth, 1)
+    pth = r"D:\projects\python\PedestrainSimulationModule\third_party\maicm\models\pedsmove\map_09_4agents_taskleave\2023_05_02_00_48_57exp_test"
+    draw_arrive_plot(pth, 2)
